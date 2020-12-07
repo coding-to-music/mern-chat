969eb102d1682f96545aa4e9fa31119bc2448865
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-07 03:41:35 -0500

    Connected with mongoDb, checked routing with postman

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 // Importing dependencies
-import express from "express"
+import express from "express";
+import mongoose from "mongoose";
 
 // App config
 const app = express()
@@ -8,6 +9,13 @@ const PORT = process.env.PORT || 9000
 // Middlewares
 
 // DB config
+const connectionUrl = "mongodb+srv://jordanwhunter:P@$$w0rd12345@cluster0.ogmha.mongodb.net/chattingdb?retryWrites=true&w=majority"
+
+mongoose.connect(connectionUrl, {
+  useCreateIndex: true,
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+})
 
 // API routes
 app.get('/', (req, res) => res.status(200).send("Hello World!"))
