47353bcf70737d45523453a50f6ef6c6d1dfefc0
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-06 22:04:59 -0500

    Added firebaseConfig ()

--- src/firebase.js
+++ src/firebase.js
@@ -1,1 +1,10 @@
 // While this is a MERN stack build (React = frontend / express, node, mongodb on the backend), Firebase will be utilized for: authentication and hosting on the frontend
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCEeif5-A9bCAuveBBIp7OqQP0RNp3eUR8",
+  authDomain: "mern-chat-9bf8f.firebaseapp.com",
+  projectId: "mern-chat-9bf8f",
+  storageBucket: "mern-chat-9bf8f.appspot.com",
+  messagingSenderId: "714101329157",
+  appId: "1:714101329157:web:bd85b7238af3d9ae577b6c"
+};
