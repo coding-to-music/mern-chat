264cbf65d6b310de59bdf7ec831efd6d13e02b69
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-06 22:28:13 -0500

    Commented out h1 tag

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Sidebar from './components/Sidebar';
 function App() {
   return (
     <div className="app">
-      <h1>Let's build a MERN Chat App</h1>
+      {/* <h1>Let's build a MERN Chat App</h1> */}
 
       {/* Sidebar component */}
       <Sidebar />
