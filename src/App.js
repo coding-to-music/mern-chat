f1ee97a6adf1b9fd58d1820edcf4d5af3bc8643f
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-06 22:41:01 -0500

    Wrapped components in div

--- src/App.js
+++ src/App.js
@@ -1,20 +1,22 @@
 import React from 'react';
-import './App.css';
 import Chat from './components/Chat/index.js';
 import Sidebar from './components/Sidebar/index.js';
+import './App.css';
 
 function App() {
   return (
     <div className="app">
       {/* <h1>Let's build a MERN Chat App</h1> */}
+      <div className="app__body">
+        {/* Sidebar component */}
+        <Sidebar />
 
-      {/* Sidebar component */}
-      <Sidebar />
-
-      {/* Chat component */}
-      <Chat />
+        {/* Chat component */}
+        <Chat />
+      </div>
     </div>
   );
 }
 
 export default App;
+ 
