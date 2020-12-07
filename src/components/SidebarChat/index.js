89b5460b3c73d59f129631864d1e96fbaa44f231
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-07 00:15:44 -0500

    Creating containers for elements of sidebarChat__info

--- src/components/SidebarChat/index.js
+++ src/components/SidebarChat/index.js
@@ -1,9 +1,15 @@
+import { Avatar } from '@material-ui/core';
 import React from 'react';
 import "./style.css";
 
 const SidebarChat = () => {
   return (
-    <div>
+    <div className="sidebarChat">
+      <Avatar />
+      <div className="sidebarChat__info">
+        <h2>Room name</h2>
+        <p>This is the last message</p>
+      </div>
       
     </div>
   )
