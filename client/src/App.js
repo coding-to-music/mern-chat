44bcaf9d22687548c11b63d680db955ce45a41f6
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-07 09:42:39 -0500

    useEffect hook for binding and subscribing to pusher channel 'messages'

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Chat from './components/Chat/index.js';
 import Sidebar from './components/Sidebar/index.js';
 import './App.css';
 
 function App() {
+
+  // runs a piece of code when the app loads
+  useEffect(() => {
+    // Copied from Pusher.com docs
+    const pusher = new Pusher('b238ba50a5658ab9e0fe', {
+      cluster: 'us2'
+    });
+
+    // We bind to event and subscribe to the pusher channel
+    const channel = pusher.subscribe('messages');
+    channel.bind('inserted', function(data) {
+      alert(JSON.stringify(data));
+    });
+  // runs piece of code once
+  }, [])
+
   return (
     <div className="app">
       {/* <h1>Let's build a MERN Chat App</h1> */}
