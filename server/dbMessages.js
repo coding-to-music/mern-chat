bdc8d8ec707e2b1dba29bf7f53f067ba472bf130
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-07 03:55:22 -0500

    Created schema

---
+++ server/dbMessages.js
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const chattingSchema = mongoose.Schema({
+  message: String,
+  name: String, 
+  timestamp: String,
+  // Currently using name to tell computer whether or not the user sent a message, but could also set up a received: String schema
+});
+
+export default mongoose.model("messageContent", chattingSchema)
