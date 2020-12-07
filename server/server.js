21a9e22c9fc7d75fe9645e8ed974ccc9de47a7bc
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-07 09:32:51 -0500

    Refactor

--- server/server.js
+++ server/server.js
@@ -3,11 +3,13 @@ import express from "express";
 import mongoose from "mongoose";
 import Pusher from "pusher";
 import Messages from "./dbMessages.js";
+import cors from "cors";
 
 // App config
 const app = express()
 const PORT = process.env.PORT || 9000
 
+// Taken from pusher.com
 const pusher = new Pusher({
   appId: "1119239",
   key: "b238ba50a5658ab9e0fe",
@@ -18,6 +20,16 @@ const pusher = new Pusher({
 
 // Middlewares
 app.use(express.json())
+app.use(cors())
+
+// NPM installed Cors as an alternative to this:
+// app.use((req, res, next) => {
+//   // Course headers - allowing the request to come from any endpoint
+//   res.setHeader("Access-Control-Allow-Origin", "*");
+//   res.setHeader("Access-Control-Allow-Headers", "*"); 
+//   // Once req is run, we push res to next()
+//   next();
+// });
 
 // DB config
 const connectionUrl = "mongodb+srv://jordanwhunter:A95R2ajtkMZ40yzk@cluster0.ogmha.mongodb.net/chattingdb?retryWrites=true&w=majority"
@@ -27,6 +39,33 @@ mongoose.connect(connectionUrl, {
   useNewUrlParser: true,
   useUnifiedTopology: true
 })
+
+const db = mongoose.connection
+
+db.once("open", () => {
+  console.log("DB connected");
+
+  const msgCollection = db.collection("messagecontents")
+  const changeStream = msgCollection.watch()
+
+  // changeStream will enact pusher to send back to front end
+  changeStream.on("change", (change) => {
+    console.log(change);
+
+    if (change.operationType === "insert") {
+      const messageDetails = change.fullDocument;
+      pusher.trigger("messages", "inserted",
+        {
+          // .name and not .user to return name of person sending message
+          name: messageDetails.name,
+          message: messageDetails.message
+        }
+      )
+    } else {
+      console.log("There was an error triggering Pusher")
+    }
+  })
+});
 
 // API routes
 app.get('/', (req, res) => res.status(200).send("Hello World!"));
