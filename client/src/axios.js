8f67270ffd0e5b1d1dfc54eef5b6ceb3bf453182
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-07 16:22:00 -0500

    Added psuedo-code for get/post instance

--- client/src/axios.js
+++ client/src/axios.js
@@ -1,7 +1,8 @@
 import axios from "axios";
 
+// create an instance so that every time we do a get or post request it has this base URL
 const instance = axios.create({
   baseURL: "http://localhost:9000"
 });
 
-export default instance;
+export default instance; 
