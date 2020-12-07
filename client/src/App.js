325290dda64a510d008f1b9eab053c989e3e4ac8
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-07 15:03:42 -0500

    Prop drilling messages

--- client/src/App.js
+++ client/src/App.js
@@ -25,11 +25,21 @@ function App() {
 
     // We bind to event and subscribe to the pusher channel
     const channel = pusher.subscribe('messages');
-    channel.bind('inserted', function(data) {
-      alert(JSON.stringify(data));
+    channel.bind('inserted', function(newMessage) {
+      // alert(JSON.stringify(newMessage));
+      // when pusher notifies of a new message - run through ALL messages and output new ones
+      setMessages([...messages, newMessage])
     });
+
+    // we need a cleanup function so a new listener isn't created every single time a message is sent
+    // the objective is to only have one subscriber when messages changes
+    return () => {
+      channel.unbind_all();
+      channel.unsubscribe();
+    }
   // runs piece of code once
-  }, [])
+  // messages needs to be added as dependency bc we're depending on it with the spread operator above
+  }, [messages])
 
   console.log(messages);
 
@@ -41,7 +51,8 @@ function App() {
         <Sidebar />
 
         {/* Chat component */}
-        <Chat />
+        {/* Prop drilling - add messages to Chat() */}
+        <Chat messages={messages}/>
       </div>
     </div>
   );
