6458a0d4af788ced8dbf34014bae7da5e568c749
jordanwhunter <jordanwhunter.mail@gmail.com>
2020-12-07 04:21:18 -0500

    Added middlewares

--- server/server.js
+++ server/server.js
@@ -1,12 +1,14 @@
 // Importing dependencies
 import express from "express";
 import mongoose from "mongoose";
+import Messages from "./dbMessages.js";
 
 // App config
 const app = express()
 const PORT = process.env.PORT || 9000
 
 // Middlewares
+app.use(express.json())
 
 // DB config
 const connectionUrl = "mongodb+srv://jordanwhunter:A95R2ajtkMZ40yzk@cluster0.ogmha.mongodb.net/chattingdb?retryWrites=true&w=majority"
@@ -18,7 +20,29 @@ mongoose.connect(connectionUrl, {
 })
 
 // API routes
-app.get('/', (req, res) => res.status(200).send("Hello World!"))
+app.get('/', (req, res) => res.status(200).send("Hello World!"));
+
+app.get('/messages/sync', (req, res) => {
+  Messages.find((err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(200).send(data)
+    }
+  })
+});
+
+app.post('/messages/new', (req, res) => {
+  const dbMessage = req.body
+
+  Messages.create(dbMessage, (err, data) => {
+    if (err) {
+      res.status(500).send(err)
+    } else {
+      res.status(201).send(`New message created: \n ${data}`)
+    }
+  })
+});
 
 // Listener
 app.listen(PORT, () => console.log(`Listening on localhost:${PORT}`));
