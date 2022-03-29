187a855a39882a11d6ad4ea90530fcdd6cc60546
coding-to-music <connors.tom@gmail.com>
2022-03-29 07:15:24 +0000

    Heroku

--- server/index.js
+++ server/index.js
@@ -71,8 +71,19 @@ db.once("open", () => {
   });
 });
 
-// API routes
-app.get("/", (req, res) => res.status(200).send("Hello World!"));
+// Priority serve any static files.
+app.use(express.static(path.resolve(__dirname, "../client/build")));
+
+// Answer API requests.
+app.get("/api", function (req, res) {
+  res.set("Content-Type", "application/json");
+  res.send('{"message":"Hello from the custom server!"}');
+});
+
+// All remaining requests return the React app, so it can handle routing.
+app.get("*", function (request, response) {
+  response.sendFile(path.resolve(__dirname, "../client/build", "index.html"));
+});
 
 app.get("/messages/sync", (req, res) => {
   Messages.find((err, data) => {
